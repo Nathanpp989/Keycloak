22387c9ed8ca7158f171d96030b862f90130785a
katzmann835 <katzmann@umich.edu>
2026-06-22 13:12:23 -0400

    Created the testcase file psuedocode

---
+++ auth0_test.py
@@ -0,0 +1,15 @@
+# This file will contain test cases relating to the rest of the code
+
+# Testing psuedocode
+
+# This section of the code will test various properties of main.py to see if it works
+
+# This part of the code will show if the various properties of authorize.py works
+
+# This part of the code will show if the auth0_talk.py properties work as intended
+
+# This part of the code will show if the auth0_type.py properties work as intended
+
+# This part of the code will show if the auth0_connect.py properties work as intended
+
+# Ultimately, this should create testcases to see if it is creating users, and ensuring that the code has been implemented correctly
